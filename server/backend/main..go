d5cff02b51577ff1463211cd40735b71fc4997d8
Vladislave <veryneedapp@gmail.com>
2022-06-23 16:00:02 +0300

    feat/init-mongo-back

---
+++ server/backend/main..go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var collection *mongo.Collection
+var ctx = context.TODO()
+
+func init() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:8000/")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
